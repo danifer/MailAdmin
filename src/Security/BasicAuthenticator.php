0d721840bb10f8cc70e9857fa3aea20677e89333
Keeton Harrington <keeton@mycertainpath.com>
2024-11-19 19:40:35 -0600

    Authentication

--- src/Security/BasicAuthenticator.php
+++ src/Security/BasicAuthenticator.php
@@ -2,49 +2,10 @@
 
 namespace App\Security;
 
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
-use Symfony\Component\Security\Core\Exception\AuthenticationException;
-use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
-use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
-use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
-use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
-use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Http\Authenticator\HttpBasicAuthenticator;
 
-class BasicAuthenticator extends AbstractAuthenticator
+class BasicAuthenticator extends HttpBasicAuthenticator
 {
-    public function supports(Request $request): ?bool
-    {
-        return $request->headers->has('PHP_AUTH_USER') && $request->headers->has('PHP_AUTH_PW');
-    }
-
-    public function authenticate(Request $request): Passport
-    {
-        $username = $request->headers->get('PHP_AUTH_USER');
-        $password = $request->headers->get('PHP_AUTH_PW');
-
-        if (null === $username) {
-            throw new CustomUserMessageAuthenticationException('Username required');
-        }
-
-        return new Passport(
-            new UserBadge($username),
-            new PasswordCredentials($password)
-        );
-    }
-
-    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
-    {
-        return null;
-    }
-
-    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
-    {
-        $response = new Response();
-        $response->headers->set('WWW-Authenticate', 'Basic realm="Secured Area"');
-        $response->setStatusCode(Response::HTTP_UNAUTHORIZED);
-
-        return $response;
-    }
 }
